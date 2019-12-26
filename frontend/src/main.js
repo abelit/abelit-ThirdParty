f809cca6507e83ca76914dd824edb3a37dc9e6d7
wiidan <snidan@gmail.com>
2019-12-26 18:03:04 +0800

    update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import vuetify from './plugins/vuetify';
-import '@babel/polyfill';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import vuetify from "./plugins/vuetify";
+import "@babel/polyfill";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
 
 // import marterial icon fonts designed by google
 import "material-design-icons-iconfont/dist/material-design-icons.css";
@@ -12,10 +12,9 @@ import "material-design-icons-iconfont/dist/material-design-icons.css";
 // import material design icon fonts
 import "@mdi/font/css/materialdesignicons.css";
 
-
 // import axios for ajax application
 import axios from "axios";
-axios.defaults.baseURL = "http://10.46.100.220:5000/";
+axios.defaults.baseURL = "http://10.10.10.200:55001/";
 Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false;
--- frontend/src/views/question/EqDce.vue
+++ frontend/src/views/question/EqDce.vue
@@ -7,22 +7,29 @@
         >
       </v-col>
     </v-row>
-    <v-row v-for="item in decQuestion" :key="item.name">
-      <v-col cols="12" v-if="item.Block == block && item.Name == name">
+    <v-row v-for="item in dceQuestion" :key="item.id">
+      <v-col cols="12" v-if="item.Block == block && item.id == id">
         <v-row align="center" justify="center">
           <v-col cols="4">
             <v-card
               style="height:200px"
               class="pa-3"
-              checked:false
-              v-on:click="greet"
+              :class="answer == item.AnswerA ? 'primary' : ''"
+              v-model="answer"
+              @click="chooseAnswer(item.AnswerA)"
             >
               <h3>{{ item.AnswerA }}</h3>
               <p>{{ item.SourceA }}</p>
             </v-card>
           </v-col>
           <v-col cols="4">
-            <v-card style="height:200px" class="pa-3" @click="changeCss(item)">
+            <v-card
+              style="height:200px"
+              class="pa-3"
+              :class="answer == item.AnswerB ? 'primary' : ''"
+              v-model="answer"
+              @click="chooseAnswer(item.AnswerB)"
+            >
               <h3>{{ item.AnswerB }}</h3>
               <p>{{ item.SourceB }}</p>
             </v-card>
@@ -40,34 +47,64 @@
   </v-container>
 </template>
 <script>
-import dataDce from "@/assets/data/dce.json";
+//import dataDce from "@/assets/data/dce.json";
+import { mapState } from "vuex";
 export default {
-  name: "EqDce",
-  props: ["block1", "startTime"],
+  // name: "EqDce",
+  // props: ["block1", "startTime"],
   data: () => ({
-    decQuestion: [{}],
+    dceQuestion: [{}],
     block: 1,
     itemOrder: 0,
-    name: dataDce[0].Name
+    name: "",
+    answer: ""
   }),
   created() {
-    this.decQuestion = dataDce;
+    this.getdceQuestion();
+
+    console.log(this.dceQuestion);
   },
   mounted() {
-    console.log(this.decQuestion);
+    //console.log(this.dceQuestion);
+  },
+  computed: {
+    ...mapState(["userInfo"])
   },
   methods: {
     nextBtn() {
-      this.itemOrder++;
-      console.log(dataDce[this.itemOrder].Name);
-      if (dataDce[this.itemOrder].Name == undefined) {
-        alert("ssss");
-      } else {
-        this.name = dataDce[this.itemOrder].Name;
-      }
+      // this.itemOrder++;
+      console.log(this.answer);
+
+      // console.log(dataDce[this.itemOrder].Name);
+      // if (dataDce[this.itemOrder].Name == undefined) {
+      //   alert("ssss");
+      // } else {
+      //   this.name = dataDce[this.itemOrder].Name;
+      // }
     },
     changeCss(item) {
       item.selected = true;
+    },
+    chooseAnswer(item) {
+      this.answer = item;
+      console.log(this.answer);
+    },
+    getdceQuestion() {
+      // console.log(this.userInfo.blockQuestion);
+      this.$axios
+        .get("/api/question/dce", {
+          params: { block: this.userInfo.blockQuestion }
+        })
+        .then(res => {
+          this.dceQuestion = res.data;
+
+          // this.currentItem = this.eqTtoQuestions[0].id;
+          // console.log(this.dceQuestion);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      // console.log(this.eqTtoQuestions);
     }
   }
 };
