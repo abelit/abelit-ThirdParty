38b71a902991566a3c8cbca34dece211036a804a
Abelit <ychenid@live.com>
2020-02-29 18:02:46 +0800

    update non-stopping tto

--- frontend/src/views/question/EqNonStpTto.vue
+++ frontend/src/views/question/EqNonStpTto.vue
@@ -1,128 +1,194 @@
 <template>
-  <v-container fluid fill-height>
-    <!-- <v-row>
-      <v-icon color="primary">mdi-arrow-up-bold</v-icon>
-      <v-icon color="primary">mdi-arrow-up-bold-outline</v-icon>
-      <v-icon color="primary">mdi-arrow-down-bold</v-icon>
-      <v-icon color="primary">mdi-arrow-down-bold-outline</v-icon>
+  <div class="grey lighten-5" style="width:1440px; margin:auto">
+    <v-card justify="center" align="start">
+      <v-card-title v-show="true">
+        <v-row>
+          <v-col>
+            <v-alert dense type="info"
+              >哪一个选择比较好，生命A、生命B，还是它们大致相同？</v-alert
+            >
+          </v-col>
+        </v-row>
+      </v-card-title>
+      <v-card-text>
+        <v-row justify="center" align="center">
+          <v-col cols="2">
+            <v-row justify="center" align="center" class="font-weight-bold">
+              偏好A
+            </v-row>
+          </v-col>
+          <v-col cols="10">
+            <v-row justify="center" align="center">
+              <table border="1" cellspacing="0" cellpadding="0" ref="table1">
+                <tr style="height: 50px">
+                  <td
+                    v-for="item in 4 * topYear"
+                    :key="item"
+                    style="text-align: center; width: 24px"
+                    :class="
+                      parseInt((item - 1) / 4) % 2 == 1
+                        ? 'green lighten-3'
+                        : 'green darken-1'
+                    "
+                  ></td>
+                </tr>
+              </table>
+            </v-row>
+          </v-col>
+        </v-row>
 
-      <v-icon color="primary">mdi-equal</v-icon>
-      <v-icon color="primary">mdi-equal-box</v-icon>
-      <v-icon color="primary">mdi-view-stream</v-icon>
-    </v-row>-->
-    <v-row class="mx-0">
-      <v-row justify="center" align="center">
-        <v-col :cols="$vuetify.breakpoint.mdAndDown?1:3">
-          <v-row justify="end">偏好A</v-row>
-        </v-col>
-        <v-col :cols="$vuetify.breakpoint.mdAndDown?11:9">
-          <v-row justify="center">
-            <table border="1" cellspacing="0" cellpadding="0" ref="table1">
-              <tr style="height: 50px">
-                <td
-                  v-for="item in range(topYear)"
-                  :key="item"
-                  style="text-align: center; width: 24px"
-                  :class="parseInt((item-1)/4) % 2 == 1? initClass3 : initClass4"
-                ></td>
-              </tr>
-            </table>
-          </v-row>
-        </v-col>
-      </v-row>
-      <v-row justify="center" align="center">
-        <v-col :cols="$vuetify.breakpoint.mdAndDown?1:3">
-          <v-row justify="end">A和B基本一样</v-row>
-        </v-col>
-        <v-col :cols="$vuetify.breakpoint.mdAndDown?11:9">
-          <v-row justify="center" align="center">
-            <table border="0" cellspacing="0" cellpadding="0" ref="table1">
-              <tr style="height: 50px">
-                <td v-for="item in range(topYear+1)" :key="item" style="width: 24px">
-                  <v-icon
-                    v-if="(item % 4 == 1)"
-                    :color="parseInt((item-1)/4) % 2 == 1? 'grey' : 'grey'"
-                    @click="printInfo(item+' up')"
-                    class="pt-5"
-                  >mdi-arrow-up-bold</v-icon>
-                  <v-icon
-                    v-if="(item % 4 == 1)"
-                    :color="parseInt((item-1)/4) % 2 == 1? 'grey' : 'grey'"
-                    @click="printInfo(item+' equal')"
-                    class="pt-5"
-                  >mdi-view-stream</v-icon>
-                  <v-icon
-                    v-if="(item % 4 == 1)"
-                    :color="parseInt((item-1)/4) % 2 == 1? 'grey' : 'grey'"
-                    @click="printInfo(item+'down')"
-                    class="pt-5"
-                  >mdi-arrow-down-bold</v-icon>
-                </td>
-              </tr>
-            </table>
-          </v-row>
-        </v-col>
-      </v-row>
-      <v-row justify="center" align="center">
-        <v-col :cols="$vuetify.breakpoint.mdAndDown?1:3">
-          <v-row justify="end">偏好B</v-row>
-        </v-col>
-        <v-col :cols="$vuetify.breakpoint.mdAndDown?11:9">
-          <v-row justify="center">
-            <table border="1" cellspacing="0" cellpadding="0" ref="table1">
-              <tr style="height: 50px">
-                <td
-                  v-for="item in range(topYear)"
-                  :key="item"
-                  style="text-align: center; width: 24px"
-                  :class="parseInt((item-1)/4) % 2 == 1? initClass1 : initClass2"
-                ></td>
-              </tr>
-            </table>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-row>
-  </v-container>
+        <v-row justify="center" align="center">
+          <v-col cols="2">
+            <v-row justify="center" align="center" class="font-weight-bold">
+              A和B大致相同
+            </v-row>
+          </v-col>
+          <v-col cols="10">
+            <v-row justify="center" align="center">
+              <table
+                border="0"
+                cellspacing="0"
+                cellpadding="0"
+                ref="table2"
+                style="table-layout: fixed"
+                height="210"
+              >
+                <tr style="height: 50px">
+                  <td
+                    v-for="item in 4 * topYear + 1"
+                    :key="item"
+                    ref="iconList"
+                    style="text-align: center; width: 24px;"
+                    @mouseover="mouseOver(item)"
+                    @mouseleave="mouseLeave(item)"
+                  >
+                    <v-icon
+                      v-if="item % 4 == 1"
+                      @click="printInfo(item + ' up')"
+                      class="py-5"
+                      :color="
+                        uLarge && item == isMouseOverItem
+                          ? 'blue darken-4'
+                          : item == isMouseOverItem
+                          ? 'blue'
+                          : 'grey lighten-1'
+                      "
+                      ref="upIcon"
+                      @mouseover="uLarge = true"
+                      @mouseleave="uLarge = false"
+                      :large="item == isMouseOverItem && uLarge ? true : false"
+                      >mdi-arrow-up-bold</v-icon
+                    >
+                    <v-icon
+                      v-if="item % 4 == 1"
+                      @click="printInfo(item + ' equal')"
+                      ref="eqIcon"
+                      class="py-5"
+                      :color="
+                        eLarge && item == isMouseOverItem
+                          ? 'blue darken-4'
+                          : item == isMouseOverItem
+                          ? 'blue'
+                          : 'grey lighten-1'
+                      "
+                      @mouseover="eLarge = true"
+                      @mouseleave="eLarge = false"
+                      :large="item == isMouseOverItem && eLarge ? true : false"
+                      >mdi-view-stream</v-icon
+                    >
+                    <v-icon
+                      v-if="item % 4 == 1"
+                      @click="printInfo(item + 'down')"
+                      class="py-5"
+                      ref="dwIcon"
+                      :color="
+                        dLarge && item == isMouseOverItem
+                          ? 'blue darken-4'
+                          : item == isMouseOverItem
+                          ? 'blue'
+                          : 'grey lighten-1'
+                      "
+                      @mouseover="dLarge = true"
+                      @mouseleave="dLarge = false"
+                      :large="item == isMouseOverItem && dLarge ? true : false"
+                      >mdi-arrow-down-bold</v-icon
+                    >
+                  </td>
+                </tr>
+              </table>
+            </v-row>
+          </v-col>
+        </v-row>
+
+        <v-row justify="center" align="center">
+          <v-col cols="2">
+            <v-row justify="center" align="center">
+              <span class="font-weight-bold headline">偏好B</span>
+            </v-row>
+          </v-col>
+          <v-col cols="10">
+            <v-row justify="center" align="center">
+              <table border="1" cellspacing="0" cellpadding="0" ref="table3">
+                <tr style="height: 50px">
+                  <td
+                    v-for="item in 4 * topYear"
+                    :key="item"
+                    style="text-align: center; width: 24px"
+                    :class="
+                      parseInt((item - 1) / 4) % 2 == 1
+                        ? 'blue lighten-3'
+                        : 'blue darken-1'
+                    "
+                  ></td>
+                </tr>
+              </table>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
-/** 逻辑： 1、全选A，完成11步回答后进入20年页面，20年回答完不会返回10年页面，总共22次回答；
- * 2、第一次选A，第二次开始不选A，回答完11道题后追加问一道题，比如9年零3个月，9年零6个月；
- * 3、其他情况按步回答完11道题即可。
-
-*/
-
 export default {
-  data() {
-    return {
-      topYear: 10,
-      initClass1: "blue lighten-3",
-      initClass2: "blue darken-1",
-      initClass3: "green lighten-3",
-      initClass4: "green darken-1"
-    };
-  },
-  computed: {
-    range: function() {
-      return function(year) {
-        var arr = [];
-        for (var i = 1; i <= 4 * year; i++) {
-          arr.push(i);
-        }
-        return arr;
-      };
-    }
-  },
+  data: () => ({
+    topYear: 10,
+    isMouseOverItem: -1,
+    isMouseOverItem: -1,
+    uLarge: false,
+    eLarge: false,
+    dLarge: false
+  }),
   methods: {
     printInfo(param) {
       alert(param);
+    },
+    mouseOver(item) {
+      // if (item % 4 == 1) {
+      //   console.log(this.$refs.iconList[item - 1]);
+      //   // console.log(this.$refs.iconList[item - 1].children[0].className);
+      //   let eLenth = this.$refs.iconList[item - 1].children.length;
+      //   for (var i = 0; i < eLenth; i++) {
+      //       this.$refs.iconList[item - 1].children[i].style.color = "#3F51B5";
+      //   }
+      // }
+      this.isMouseOverItem = item;
+    },
+    mouseOverIcon(item) {
+      this.isMouseOverIcon = item;
+    },
+    mouseLeave(item) {
+      // if (item % 4 == 1) {
+      //   // console.log(this.$refs.iconList[item - 1].children.length);
+      //   let eLenth = this.$refs.iconList[item - 1].children.length;
+      //   for (var i = 0; i < eLenth; i++) {
+      //       this.$refs.iconList[item - 1].children[i].style.color = "#BDBDBD";
+      //   }
+      // }
+      this.isMouseOverItem = -1;
+      this.isMouseOverIcon = -1;
     }
   }
 };
 </script>
-
-<style lang="scss">
-.app-td {
-}
-</style>
---
+++ frontend/src/views/question/demo.vue
@@ -0,0 +1,170 @@
+<template>
+  <v-container fluid fill-height>
+    <!-- <v-row>
+      <v-icon color="primary">mdi-arrow-up-bold</v-icon>
+      <v-icon color="primary">mdi-arrow-up-bold-outline</v-icon>
+      <v-icon color="primary">mdi-arrow-down-bold</v-icon>
+      <v-icon color="primary">mdi-arrow-down-bold-outline</v-icon>
+
+      <v-icon color="primary">mdi-equal</v-icon>
+      <v-icon color="primary">mdi-equal-box</v-icon>
+      <v-icon color="primary">mdi-view-stream</v-icon>
+    </v-row>-->
+    <v-row class="mx-0">
+      <v-row justify="center" align="center">
+        <v-col :cols="$vuetify.breakpoint.mdAndDown ? 2 : 2">
+          <v-row :justify="$vuetify.breakpoint.mdAndDown ? 'center' : 'end'"
+            >偏好A</v-row
+          >
+        </v-col>
+        <v-col :cols="$vuetify.breakpoint.mdAndDown ? 10 : 9">
+          <v-row justify="center" no-getters>
+            <table
+              border="1"
+              cellspacing="0"
+              cellpadding="0"
+              ref="table1"
+              style="table-layout: fixed"
+            >
+              <tr style="height: 50px">
+                <td
+                  v-for="item in range(topYear)"
+                  :key="item"
+                  style="text-align: center; width: 24px;"
+                  :class="
+                    parseInt((item - 1) / 4) % 2 == 1 ? initClass3 : initClass4
+                  "
+                ></td>
+              </tr>
+            </table>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col :cols="$vuetify.breakpoint.mdAndDown ? 2 : 2" style="margin-left: 72px">
+          <v-row :justify="$vuetify.breakpoint.mdAndDown ? 'center' : 'end'"
+            >A和B基本一样</v-row
+          >
+        </v-col>
+        <v-col :cols="$vuetify.breakpoint.mdAndDown ? 10 : 9">
+          <v-row justify="center" align="center">
+            <table
+              border="0"
+              cellspacing="0"
+              cellpadding="0"
+              ref="table1"
+              style="table-layout: fixed"
+            >
+              <tr style="height: 50px">
+                <td
+                  v-for="item in range(topYear + 1)"
+                  :key="item"
+                  style="text-align: center; width: 24px;"
+                  @mouseover="initColor1='indigo';large=true"
+                  @mouseleave="initColor1='grey'"
+                >
+                  <v-icon
+                    v-if="item % 4 == 1"
+                    :color="initColor1"
+                    @click="printInfo(item + ' up');initColor1='red'"
+                    :large='large'
+                    dark
+                    disabled
+                    class="pt-5"
+                    >mdi-arrow-up-bold</v-icon
+                  >
+                  <v-icon
+                    v-if="item % 4 == 1"
+                    :color="initColor1"
+                    @click="printInfo(item + ' equal')"
+                    class="pt-5"
+                    >mdi-view-stream</v-icon
+                  >
+                  <v-icon
+                    v-if="item % 4 == 1"
+                    :color="initColor1"
+                    @click="printInfo(item + 'down')"
+                    class="pt-5"
+                    >mdi-arrow-down-bold</v-icon
+                  >
+                </td>
+              </tr>
+            </table>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col :cols="$vuetify.breakpoint.mdAndDown ? 2 : 2">
+          <v-row :justify="$vuetify.breakpoint.mdAndDown ? 'center' : 'end'"
+            >偏好B</v-row
+          >
+        </v-col>
+        <v-col :cols="$vuetify.breakpoint.mdAndDown ? 10 : 9">
+          <v-row justify="center">
+            <table
+              border="1"
+              cellspacing="0"
+              cellpadding="0"
+              ref="table1"
+              style="table-layout: fixed"
+            >
+              <tr style="height: 50px">
+                <td
+                  v-for="item in range(topYear)"
+                  :key="item"
+                  style="width: 24px"
+                  :class="
+                    parseInt((item - 1) / 4) % 2 == 1 ? initClass1 : initClass2
+                  "
+                ></td>
+              </tr>
+            </table>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+/** 逻辑： 1、全选A，完成11步回答后进入20年页面，20年回答完不会返回10年页面，总共22次回答；
+ * 2、第一次选A，第二次开始不选A，回答完11道题后追加问一道题，比如9年零3个月，9年零6个月；
+ * 3、其他情况按步回答完11道题即可。
+
+*/
+
+export default {
+  data() {
+    return {
+      topYear: 10,
+      initClass1: "blue lighten-3",
+      initClass2: "blue darken-1",
+      initClass3: "green lighten-3",
+      initClass4: "green darken-1",
+      initColor1: 'grey',
+      large: false
+    };
+  },
+  computed: {
+    range: function() {
+      return function(year) {
+        var arr = [];
+        for (var i = 1; i <= 4 * year; i++) {
+          arr.push(i);
+        }
+        return arr;
+      };
+    }
+  },
+  methods: {
+    printInfo(param) {
+      alert(param);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.app-td {
+}
+</style>
