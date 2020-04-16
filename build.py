39d8818a60efb25acb59382f4e20f14fce6eadda
abelit <ychenid@live.com>
2020-04-16 14:54:15 +0800

    add time range for ptraffic

---
+++ build.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+os.chdir("./frontend")
+
+os.system("npm install")
+
+os.system("npm run build")
+
+def copy_and_overwrite(from_path, to_path):
+    if os.path.exists(to_path):
+        shutil.rmtree(to_path)
+    shutil.copytree(from_path, to_path)
+
+shutil.copy('./dist/index.html','../third_party_feedback/feedback/templates/index.html')
+shutil.copy('./dist/favicon.ico','../third_party_feedback/feedback/static/favicon.ico')
+
+for i in os.listdir('./dist/static'):
+    copy_and_overwrite('./dist/static/'+i,'../third_party_feedback/feedback/static/'+i)
