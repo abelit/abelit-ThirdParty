9788ef46b58c99fbd16d8a776cd13c7e8700ebb1
abelit <ychenid@live.com>
2020-05-15 15:02:30 +0800

    add used_time for dce

--- third_party_feedback/feedback/main.py
+++ third_party_feedback/feedback/main.py
@@ -74,7 +74,7 @@ def init_db():
                 "create table ttofeedback_question(id integer primary key autoincrement,questionid integer, presentation text,type text, name text,block integer,source_text text,version text,created_timestamp timestamp default (datetime(current_timestamp, 'localtime')))",
                 "create table dce_question(id integer primary key autoincrement,questionid integer,presentation text,name integer,block integer,answer text,source_text text, version text,created_timestamp timestamp default (datetime(current_timestamp, 'localtime')))",
                 "create table opened_question(id integer primary key autoincrement,questionid integer,presentation text,name text,block text,source_text text, version text, created_timestamp timestamp default (datetime(current_timestamp, 'localtime')))",
-                "create table dce_answer(id integer primary key autoincrement,questionid integer,participant text,interviewer text,item integer, position_of_item integer,selected_state text,dce_reversal text,block integer, version text,created_timestamp timestamp default (datetime(current_timestamp, 'localtime')))",
+                "create table dce_answer(id integer primary key autoincrement,questionid integer,participant text,interviewer text,item integer, position_of_item integer,selected_state text,dce_reversal text,block integer,used_time text, version text,created_timestamp timestamp default (datetime(current_timestamp, 'localtime')))",
                 "create table tto_answer(id integer primary key autoincrement,questionid integer, participant text, interviewer text,item text,position_of_item integer,tto_value real,used_time text,composite_switches interger,resets integer,number_of_moves integer,block text,version text,created_timestamp timestamp default (datetime(current_timestamp, 'localtime')))",
                 "create table newtto_answer(id integer primary key autoincrement,questionid integer, participant text, interviewer text,item text,position_of_item integer,start_year_random text,select1 text,select2 text,select3 text,select4 text,open_select text,end_year_random text,block text,reset text,used_time text,version text,created_timestamp timestamp default (datetime(current_timestamp, 'localtime')))",
                 "create table nstptto_answer(id integer primary key autoincrement,questionid integer, participant text, interviewer text,item text,position_of_item integer,select_order interger,select_value text,page integer,block text,used_time text,version text,created_timestamp timestamp default (datetime(current_timestamp, 'localtime')))",
@@ -720,11 +720,13 @@ def add_dce_answer():
     status = None
     msg = None
 
+    print(content)
+
     try:
         cursor = conn.cursor()
         for row in content:
-            SQL_TEXT = "insert into dce_answer(questionid,participant,interviewer,item,position_of_item,selected_state,dce_reversal,block,version) values('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}')".format(
-                row['questionid'], row['participant'], row['interviewer'], row['item'], row['position_of_item'], row['selected_state'], row['dce_reversal'], row['block'], row['version'])
+            SQL_TEXT = "insert into dce_answer(questionid,participant,interviewer,item,position_of_item,selected_state,dce_reversal,block,used_time,version) values('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}','{9}')".format(
+                row['questionid'], row['participant'], row['interviewer'], row['item'], row['position_of_item'], row['selected_state'], row['dce_reversal'], row['block'],row['used_time'], row['version'])
             cursor.execute(SQL_TEXT)
             print(SQL_TEXT)
         conn.commit()
@@ -748,7 +750,7 @@ def get_dce_answer():
     cursor = conn.cursor()
 
     SQL_TEXT = "select * from dce_answer"
-    print(SQL_TEXT)
+
 
     if version is not None and version != "all":
         SQL_TEXT = SQL_TEXT + " " + "where version='{0}'".format(version)
@@ -761,13 +763,13 @@ def get_dce_answer():
             else:
                 SQL_TEXT = SQL_TEXT + " " + \
                     "and  participant='{0}'".format(participant)
-
+    print(SQL_TEXT)
     result = cursor.execute(SQL_TEXT)
     data = []
     for row in result:
         print(row)
         data.append({"id": row[0], "questionid": row[1], "participant": row[2], "interviewer": row[3], "item": row[4], "position_of_item": row[5],
-                     "selected_state": row[6], "dce_reversal": row[7], "block": row[8], "version": row[9], "created_timestamp": row[10]})
+                     "selected_state": row[6], "dce_reversal": row[7], "block": row[8], "used_time": row[9], "version": row[10], "created_timestamp": row[11]})
 
     return jsonify(data)
 
@@ -1011,293 +1013,6 @@ def get_file(filename):
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-# class Oracle:
-#     def __init__(self, username, password, host, port, instance, mode):
-#         """
-#         Function: __init__
-#         Summary: InsertHere
-#         Examples: InsertHere
-#         Attributes:
-#             @param (self):InsertHere
-#             @param (*args):InsertHere
-#             username: oracle user,
-#             password: oracle user's password,
-#             mod:  "normal, sysdba, sysoper",defaut is normal,
-#             host: the oracle database locates
-#             port: the port listen oracle database service, config is 1521
-#             insrance: the service name of the oracle database instance
-#         Returns: InsertHere
-#         """
-#         self.username = username
-#         self.password = password
-#         self.host = host
-#         self.port = port
-#         self.instance = instance
-#         self.mode = mode
-
-#     def __connect(self):
-#         """
-#         Function: __connect
-#         Summary: InsertHere
-#         Examples: self.__connect()
-#         Attributes:
-#             @param (self):Call __connect metod Oracle.__connect()
-#         Returns: connection
-#         """
-#         if not cx_oracle_exists:
-#             msg = """The cx_Oracle module is required. 'pip install cx_Oracle' \
-#                 should do the trick. If cx_Oracle is installed, make sure ORACLE_HOME \
-#                 & LD_LIBRARY_PATH is set"""
-
-#         dsn = cx_Oracle.makedsn(
-#             host=self.host, port=self.port, service_name=self.instance)
-
-#         try:
-#             if self.mode == 'sysdba' or self.username == 'sys':
-#                 self.connection = cx_Oracle.connect(self.username, self.password, dsn,
-#                                                     mode=cx_Oracle.SYSDBA)
-#             else:
-#                 self.connection = cx_Oracle.connect(
-#                     self.username, self.password, dsn)
-
-#         except cx_Oracle.DatabaseError as cx_msg:
-#             msg = 'Could not connect to database: %s, dsn: %s ' % (cx_msg, dsn)
-#         return self.connection
-
-#     def __disconnect(self):
-#         try:
-#             self.cursor.close()
-#             self.connection.close()
-#         except cx_Oracle.DatabaseError as cx_msg:
-#             print(cx_msg)
-
-#     def select(self, sql, bindvars=''):
-#         """
-#         Given a valid SELECT statement, return the results from the database
-#         """
-
-#         results = None
-
-#         try:
-#             self.__connect()
-#             self.cursor = self.connection.cursor()
-#             self.cursor.execute(sql, bindvars)
-#             results = self.cursor.fetchall()
-#         except cx_Oracle.DatabaseError as cx_msg:
-#             print(cx_msg)
-#         finally:
-#             self.__disconnect()
-#         return results
-
-#     def execute(self, sql, bindvars='', commit=True):
-#         """
-#         Function: execute
-#         Summary: Execute whatever SQL statements are passed to the method;
-#             commit if specified. Do not specify fetchall() in here as
-#             the SQL statement may not be a select.
-#             bindvars is a dictionary of variables you pass to execute.
-#         Examples: Oracle().execute(...)
-#         Attributes:
-#             @param (self):class method
-#             @param (sql):The sql that will be excuted
-#             @param (bindvars) config='': The bind variables of sql
-#             @param (many) config=False: If set many is True, multiple sql will be excuted at the same time
-#             @param (commit) config=False: False is not needed commit after excuting sql, but True is needed commit, gernerally DML sql
-#         Returns: NO value will be returned
-#         """
-
-#         try:
-#             self.__connect()
-#             self.cursor = self.connection.cursor()
-#             self.cursor.execute(sql, bindvars)
-#             print(
-#                 "The sql executing from database successfully and the sql is " + '"' + sql + '".')
-#         except cx_Oracle.DatabaseError as cx_msg:
-#             print(cx_msg)
-#         finally:
-#             # Only commit if it-s necessary.
-#             if commit:
-#                 self.connection.commit()
-#             else:
-#                 pass
-#             self.__disconnect()
-
-
-# @app.route("/api/ptraffic")
-# def get_passenger_traffic():
-#     starttime = request.args.get('start')
-#     endtime = request.args.get('end')
-
-#     if starttime is None:
-#         starttime = 0
-#     if endtime is None:
-#         endtime = 24
-#     oracle = Oracle(username='', password='',
-#                     mode="", host="10.50.0.212", port=1521, instance='')
-#     sql_text = """select pdate,pentry,pin,pout,sysdate as gettime from (SELECT
-#         a.XF_DATE_TIME pdate,
-#         b.xf_passageway pentry,
-#             sum(a.xf_incount) pin,
-#             sum(a.xf_outcount) pout
-#         from 
-#         XF_new_TC_COUNTDATA a,
-#             XF_TC_PASS b 
-#         WHERE
-#         a.XF_CAMERAID=b.XF_CAMERAID and
-#         a.XF_CAMERAID NOT IN ('GHGY5500033011','GHGY5500033012','GHGY5500033013','GHGY5500033019','GHGY5500033020','GHGY5500033022','GHGY5500033023','GHGY5500033024','GHGY5500033025',
-#         'GHGY5500033033','GHGY5500033034','GHGY5500033035','GHGY5500033036','GHGY5500033040','GHGY5500033041','GHGY5500033042','GHGY5500033043','GHGY5500033046',
-#         'GHGY5500033047','GHGY5500033048','GHGY5500033049','GHGY5500033050','GHGY5500033051','GHGY5500033052','GHGY5500033053','GHGY5500033055','GHGY5500033056',
-#         'GHGY5500033057','GHGY5500033058','GHGY5500033059','GHGY5500033060','GHGY5500033061','GHGY5500033062','GHGY5500033073','GHGY5500033074','GHGY5500033076',
-#         'GHGY5500033077','GHGY5500033078','GHGY5500033082','GHGY5500033083','GHGY5500033084','GHGY5500033096','GHGY5500033097','GHGY5500033098','GHGY5500033099',
-#         'GHGY5500033100','GHGY5500033101','GHGY5500033104','GHGY5500033105','GHGY5500033106','GHGY5500033107','GHGY5500033108','GHGY5500033109','GHGY5500033110',
-#         'GHGY5500033111','GHGY5500033114','GHGY5500033117','GHGY5500033118','GHGY5500033119','GHGY5500033128','GHGY5500033133','GHGY5500033134','GHGY5500033135',
-#         'GHGY5500033138','GHGY5500033139','GHGY5500033145','GHGY5500033148','GHGY5500033149','GHGY5500033150','GHGY5500033151','GHGY5500033152','GHGY5500033153',
-#         'GHGY5500033162','GHGY5500033163','GHGY5500033175','GHGY5500033176','GHGY5500033177','GHGY5500033178','GHGY5500033181','GHGY5500033183','GHGY5500033184',
-#         'GHGY5500033185','GHGY5500033186','GHGY5500033187','GHGY5500033188','GHGY5500033189','GHGY5500033192','GHGY5500033193','GHGY5500033198','GHGY5500033199',
-#         'GHGY5500033200','GHGY5500033204','GHGY5500033205','GHGY5500033210','GHGY5500033211','GHGY5500033212','GHGY5500033213','GHGY5500033214','GHGY5500033215',
-#         'GHGY5500033216','GHGY5500033217','GHGY5500033218','GHGY5500033219','GHGY5500033220','GHGY5500033221','GHGY5500033228') AND
-#          to_date(a.XF_DATE_TIME,'yyyy-mm-dd') = to_date(to_char(sysdate,'yyyy-mm-dd'),'yyyy-mm-dd') and XF_STARTHOUR between {0} and {1}
-#         GROUP BY
-#         a.XF_DATE_TIME,b.xf_passageway
-#         order by a.XF_DATE_TIME)"""
-#     results = oracle.select(sql_text.format(starttime, endtime))
-
-#     data = []
-
-#     for row in results:
-#         print(row[1])
-#         data.append({
-#             "pdate": row[0],
-#             "pentry": row[1],
-#             "pin": row[2],
-#             "pout": row[3],
-#             "gettime": row[4]
-#         })
-
-#     return jsonify(data)
-
-
-# @app.route("/api/ptraffic/all")
-# def get_all_passenger_traffic():
-#     oracle = Oracle(username='', password='',
-#                     mode="", host="10.50.0.212", port=1521, instance='')
-#     sql_text = """select pdate,pentry,pin,pout,sysdate as gettime from (SELECT
-#         a.XF_DATE_TIME pdate,
-#         b.xf_passageway pentry,
-#             sum(a.xf_incount) pin,
-#             sum(a.xf_outcount) pout
-#         from 
-#         XF_new_TC_COUNTDATA a,
-#             XF_TC_PASS b 
-#         WHERE
-#         a.XF_CAMERAID=b.XF_CAMERAID and
-#         a.XF_CAMERAID NOT IN ('GHGY5500033011','GHGY5500033012','GHGY5500033013','GHGY5500033019','GHGY5500033020','GHGY5500033022','GHGY5500033023','GHGY5500033024','GHGY5500033025',
-#         'GHGY5500033033','GHGY5500033034','GHGY5500033035','GHGY5500033036','GHGY5500033040','GHGY5500033041','GHGY5500033042','GHGY5500033043','GHGY5500033046',
-#         'GHGY5500033047','GHGY5500033048','GHGY5500033049','GHGY5500033050','GHGY5500033051','GHGY5500033052','GHGY5500033053','GHGY5500033055','GHGY5500033056',
-#         'GHGY5500033057','GHGY5500033058','GHGY5500033059','GHGY5500033060','GHGY5500033061','GHGY5500033062','GHGY5500033073','GHGY5500033074','GHGY5500033076',
-#         'GHGY5500033077','GHGY5500033078','GHGY5500033082','GHGY5500033083','GHGY5500033084','GHGY5500033096','GHGY5500033097','GHGY5500033098','GHGY5500033099',
-#         'GHGY5500033100','GHGY5500033101','GHGY5500033104','GHGY5500033105','GHGY5500033106','GHGY5500033107','GHGY5500033108','GHGY5500033109','GHGY5500033110',
-#         'GHGY5500033111','GHGY5500033114','GHGY5500033117','GHGY5500033118','GHGY5500033119','GHGY5500033128','GHGY5500033133','GHGY5500033134','GHGY5500033135',
-#         'GHGY5500033138','GHGY5500033139','GHGY5500033145','GHGY5500033148','GHGY5500033149','GHGY5500033150','GHGY5500033151','GHGY5500033152','GHGY5500033153',
-#         'GHGY5500033162','GHGY5500033163','GHGY5500033175','GHGY5500033176','GHGY5500033177','GHGY5500033178','GHGY5500033181','GHGY5500033183','GHGY5500033184',
-#         'GHGY5500033185','GHGY5500033186','GHGY5500033187','GHGY5500033188','GHGY5500033189','GHGY5500033192','GHGY5500033193','GHGY5500033198','GHGY5500033199',
-#         'GHGY5500033200','GHGY5500033204','GHGY5500033205','GHGY5500033210','GHGY5500033211','GHGY5500033212','GHGY5500033213','GHGY5500033214','GHGY5500033215',
-#         'GHGY5500033216','GHGY5500033217','GHGY5500033218','GHGY5500033219','GHGY5500033220','GHGY5500033221','GHGY5500033228') AND
-#          to_date(a.XF_DATE_TIME,'yyyy-mm-dd') = to_date(to_char(sysdate,'yyyy-mm-dd'),'yyyy-mm-dd')
-#         GROUP BY
-#         a.XF_DATE_TIME,b.xf_passageway
-#         order by a.XF_DATE_TIME)"""
-#     results = oracle.select(sql_text)
-
-#     data = []
-
-#     for row in results:
-#         print(row[1])
-#         data.append({
-#             "pdate": row[0],
-#             "pentry": row[1],
-#             "pin": row[2],
-#             "pout": row[3],
-#             "gettime": row[4]
-#         })
-
-#     return jsonify(data)
-
-
-
-# @app.route("/api/ptraffic/time")
-# def get_time_passenger_traffic():
-#     starttime = request.args.get('start')
-#     endtime = request.args.get('end')
-
-#     if starttime is None:
-#         starttime = 0
-#     if endtime is None:
-#         endtime = 24
-#     oracle = Oracle(username='', password='',
-#                     mode="", host="10.50.0.212", port=1521, instance='')
-#     sql_text = """SELECT
-#         min(xf_starthour)||':'||'00'||'-'||max(xf_starthour)||':'||'00' 时间段,
-#         nvl(sum(case
-#         when XF_CAMERAID in 
-#             ('GHGY5500033140','GHGY5500033141','GHGY5500033142','GHGY5500033143','GHGY5500033144','GHGY5500033147',
-#             'GHGY5500033155','GHGY5500033156','GHGY5500033157','GHGY5500033158','GHGY5500033159',
-#             'GHGY5500033169','GHGY5500033170','GHGY5500033171','GHGY5500033172','GHGY5500033173','GHGY5500033174','GHGY5500033168')
-#             then XF_INCOUNT  
-#             end),0) "沿湖广场",
-#         nvl(sum(case
-#         when XF_CAMERAID in 
-#             ('GHGY5500033026','GHGY5500033027','GHGY5500033028','GHGY5500033029','GHGY5500033030','GHGY5500033031','GHGY5500033032',
-#             'GHGY5500033089','GHGY5500033090','GHGY5500033091','GHGY5500033092','GHGY5500033093','GHGY5500033094','GHGY5500033095')
-#             then XF_INCOUNT 
-#             end),0) "H&M",
-#         nvl(sum(case
-#         when XF_CAMERAID in 
-#             ('GHGY5500033230','GHGY5500033229','GHGY5500033196','GHGY5500033197')
-#             then XF_INCOUNT  
-#             end),0) "人行天桥",
-#         nvl(sum(case
-#         when XF_CAMERAID in 
-#             ('GHGY5500033002','GHGY5500033003','GHGY5500033004','GHGY5500033005','GHGY5500033006','GHGY5500033007','GHGY5500033008','GHGY5500033009','GHGY5500033010',
-#             'GHGY5500033014','GHGY5500033015','GHGY5500033016','GHGY5500033017','GHGY5500033018')
-#             then XF_INCOUNT  
-#             end),0) "停车场",
-#         nvl(sum(case
-#         when XF_CAMERAID in
-#             ('GHGY5500033002','GHGY5500033003','GHGY5500033004','GHGY5500033005','GHGY5500033006','GHGY5500033007','GHGY5500033008','GHGY5500033009','GHGY5500033010',
-#             'GHGY5500033014','GHGY5500033015','GHGY5500033016','GHGY5500033017','GHGY5500033018',
-#             'GHGY5500033026','GHGY5500033027','GHGY5500033028','GHGY5500033029','GHGY5500033030','GHGY5500033031','GHGY5500033032',
-#             'GHGY5500033089','GHGY5500033090','GHGY5500033091','GHGY5500033092','GHGY5500033093','GHGY5500033094','GHGY5500033095',
-#             'GHGY5500033140','GHGY5500033141','GHGY5500033142','GHGY5500033143','GHGY5500033144','GHGY5500033147',
-#             'GHGY5500033155','GHGY5500033156','GHGY5500033157','GHGY5500033158','GHGY5500033159',
-#             'GHGY5500033169','GHGY5500033170','GHGY5500033171','GHGY5500033172','GHGY5500033173','GHGY5500033174','GHGY5500033168',
-#             'GHGY5500033230','GHGY5500033229','GHGY5500033196','GHGY5500033197')
-#             then XF_INCOUNT  
-#             end),0) "共计"
-#         from 
-#         xf_tc_countdata
-#         WHERE
-#         xf_starthour between 10 and 22 and
-#         to_date(XF_DATE_TIME,'yyyy-mm-dd') between to_date({0},'yyyy-mm-dd') and to_Date({1},'yyyy-mm-dd')
-#         """
-#     results = oracle.select(sql_text.format(starttime, endtime))
-
-#     data = []
-
-#     for row in results:
-#         print(row[1])
-#         data.append({
-#             "pdate": row[0],
-#             "psquare": row[1],
-#             "phm": row[2],
-#             "pgateway": row[3],
-#             "pparking": row[4],
-#             "pall": row[5]
-#         })
-
-#     return jsonify(data)
 
 
 if __name__ == "__main__":
